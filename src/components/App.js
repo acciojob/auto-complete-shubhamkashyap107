b023356b428bf49adac16a2a6a2b0ab94fc12ad2
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-09-03 19:16:57 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
 const App = () => {
+
+  const[data, setData] = useState(fruits)
+  const[filteredData, setFilteredData] = useState(fruits)
+  
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input onChange={(e) => {
+          const arr = data.filter((item) => {
+            return item.includes(e.target.value)
+          })
+          setFilteredData(arr)
+        }} />
+        <ul>
+          {filteredData.map((item) => {
+            return <li>{item}</li>
+          })}
+        </ul>
     </div>
   )
 }
